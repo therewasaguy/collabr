0a87efefce12be589343b2f49fc373d00e289d8a
Jason Sigal <jason.sigal@gmail.com>
2014-04-08 20:15:29 -0400

    adding __dirname

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 fs = require('fs');
 connect = require('connect');
 
+console.log(__dirname);
+
 var app = connect()
   .use(connect.bodyParser())
-  .use(connect.static('public'))
+  .use(connect.static(__dirname + '/public'))
   .use(function (req, res) {
     if (req.url === '/process') {
       res.end(req.body.name + ' would repeat ' + req.body.repeat + ' times.');
     } else {
-      res.end("Invalid Request");
+      res.end(req.url+' is not a valid page.');
     }
   })
   .listen(3000);
