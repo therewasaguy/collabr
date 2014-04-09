177d028f55200fd32af7f8978354f46d5076c9fb
Jason Sigal <jason.sigal@gmail.com>
2014-04-08 21:04:04 -0400

    trying to handle Heroku application error

--- server.js
+++ server.js
@@ -16,10 +16,10 @@ var app = connect()
 
     if (url){
       fs.readFile(url, function(err, data) {
-        // if (err) {
-        //   res.writeHead(404);
-        //   return res.end("File not found.");
-        // }
+        if (err) {
+          res.writeHead(404);
+          return res.end("File not found.");
+        }
 
         res.setHeader("Content-Type", mime.lookup(url));
         res.writeHead(200);
